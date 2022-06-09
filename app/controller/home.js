3217ae29680363b839420d1273290bf94dea718e
jianxinhu <jianxin.hu@deeper.network>
2022-06-09 14:38:43 +0800

    feature: update ip and ua tracking

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,11 +2,10 @@
 
 const Controller = require('egg').Controller;
 
-// const parser = require('ua-parser-js');
-// const libQQwry = require('lib-qqwry');
-
-// libQQwry.init();
-// libQQwry.speed();
+const parser = require('ua-parser-js');
+const libQQwry = require('lib-qqwry');
+const qqwry = libQQwry.init();
+qqwry.speed();
 
 class HomeController extends Controller {
   async index() {
@@ -19,18 +18,17 @@ class HomeController extends Controller {
       query,
     } = this.ctx.request;
 
-    // const ua = parser(this.ctx.header('user-agent'));
+    const ua = parser(this.ctx.headers['user-agent']);
+    const { ip } = qqwry.searchIP(this.ctx.request.ip);
 
-    // const ip = libQQwry.searchIP(this.ctx.request.ip);
-
-    // console.log(`ip:${ip}`);
-    // console.log(`ua:${JSON.stringify(ua)}`);
+    console.log(`ip:${JSON.stringify(ip)}`);
+    console.log(`ua:${JSON.stringify(ua)}`);
 
     // console.log('GET!');
     const imageBufferData = Buffer.alloc(1);
     await this.ctx.service.deeperEvents.save({
-      // ip,
-      // ua,
+      ip,
+      ua: JSON.stringify(ua),
       ...query,
     });
     this.ctx.body = imageBufferData;
